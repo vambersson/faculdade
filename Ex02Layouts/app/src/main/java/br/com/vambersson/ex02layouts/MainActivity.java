c7e2a050396a9b07d8bbb3cda62fccf447b22cf9
vambersson <TIUniao@uniaope.local>
2017-03-28 10:51:18 -0300

    Exemplo 02 Layouts
    Tipos de Layouts

---
+++ Ex02Layouts/app/src/main/java/br/com/vambersson/ex02layouts/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.vambersson.ex02layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    //android.support.constraint.ConstraintLayout
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
