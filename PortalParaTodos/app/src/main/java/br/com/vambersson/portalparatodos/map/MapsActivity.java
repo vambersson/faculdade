0aaa5727bafba31c02141c16537196837b725c56
Derick Irineu <derickirineu@gmail.com>
2017-06-12 13:33:08 -0300

    Alterações e implementações na activity para integração com o google maps

--- PortalParaTodos/app/src/main/java/br/com/vambersson/portalparatodos/map/MapsActivity.java
+++ PortalParaTodos/app/src/main/java/br/com/vambersson/portalparatodos/map/MapsActivity.java
@@ -2,6 +2,7 @@ package br.com.vambersson.portalparatodos.map;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -25,47 +26,48 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+
+import java.util.List;
 
 import br.com.vambersson.portalparatodos.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+        LocationListener,
+        Rotas.OnRotaCriadaListner{
+
     private GoogleMap mMap;
+    private LatLng localAtual;
+    private LatLng prefeitura;
+    private Marker marcador;
+    private Localizacao localizacao;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
     @Override
     public void onLocationChanged(Location location) {
+
+        localAtual = new LatLng(location.getLatitude(),location.getLongitude());
+
+        if(marcador != null){
+            marcador.setPosition(localAtual);
+        }else{
+            marcador = mMap.addMarker(new MarkerOptions().position(localAtual).title("Local Atual"));
+        }
+
+        new Rotas(this, prefeitura, localAtual).execute();
 
     }
 
@@ -73,9 +75,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-8.16527445, -34.92120266);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        localizacao = new Localizacao(this, this);
+
+        prefeitura = new LatLng(-8.0555904,-34.8723802);
+        mMap.addMarker(new MarkerOptions().position(prefeitura).title("Prefeitura"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(prefeitura, 20.0f));
+
+    }
+
+    @Override
+    public void rotaCriada(List<LatLng> rotas) {
+
+        if(rotas != null && rotas.size() > 0){
+            PolylineOptions polylineOptions = new PolylineOptions()
+                    .addAll(rotas)
+                    .width(5)
+                    .color(Color.RED)
+                    .visible(true);
+
+            mMap.addPolyline(polylineOptions);
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(localAtual));
     }
 }
